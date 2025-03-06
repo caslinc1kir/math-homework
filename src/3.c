e8adbec7c62618c77a08b634119ee3da43df948c
caslinc1kir <caslinc1kir@gmx.com>
2025-03-06 14:04:28 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,9 @@
+int main() {
+    // Calculate the area of a circle with a radius of 5
+    float area = 3.14 * (5 * 5);
+
+    // Print the result to the console
+    printf("The area of the circle is: %f", area);
+
+    return 0;
+}
